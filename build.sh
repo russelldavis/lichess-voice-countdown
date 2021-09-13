b3b30feaf847bba2dc1f1489828270c324c7da04
Russell Davis <551404+russelldavis@users.noreply.github.com>
2021-09-12 19:02:08 -0700

    Simplify

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/bash
-set -PEeuo pipefail
-shopt -s failglob inherit_errexit
-
-cd ext
-rm  -f ../dist.zip
-zip -r ../dist.zip .
--- ext/content.js
+++ ext/content.js
@@ -1,62 +1,80 @@
-import { getOptions, getVoices, speak, voicePredicate } from "./lib.js";
 const $ = document.querySelector.bind(document);
 
-class Handler {
-  constructor(voice, extOpts) {
-    this.voice = voice;
-    this.extOpts = extOpts;
-    this.prevSeconds = null;
+const OPTIONS = {
+  alertTimes: [45, 30, 15, 10, 5, 2],
+  voiceName: "Alex",
+}
+
+const voices = await getVoices();
+const voice = voices.find((voice) => voice.name === OPTIONS.voiceName);
+let prevSeconds = null;
+
+function speak(text) {
+  console.log(`Speaking: ${text}`);
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.voice = voice;
+  speechSynthesis.speak(utterance);
+}
+
+function getVoices() {
+  return new Promise((resolve, _reject) => {
+    const voices = speechSynthesis.getVoices();
+    if (voices.length !== 0) {
+      resolve(voices);
+    } else {
+      const listener = () => {
+        speechSynthesis.removeEventListener("voiceschanged", listener);
+        resolve(speechSynthesis.getVoices());
+      };
+      speechSynthesis.addEventListener("voiceschanged", listener);
+    }
+  });
+}
+
+
+function onTimeMutation(timeStr) {
+  const match = timeStr.match(/(\d\d)\s*[^\d]\s*(\d\d)\s*([^\d]\d*)?$/);
+  if (!match) {
+    console.log("Can't parse time:", timeStr);
+    return;
   }
-
-  onTimeMutation(timeStr) {
-    const match = timeStr.match(/(\d\d)\s*[^\d]\s*(\d\d)\s*([^\d]\d*)?$/);
-    if (!match) {
-      console.log("Can't parse time:", timeStr);
-      return;
-    }
-    const seconds = (Number(match[1]) * 60) + Number(match[2]);
-    if (this.prevSeconds == null) {
-      // It's the first render. If the starting time happens to fall on an alert marker,
-      // no need to alert at the start of the game. This also avoids alerting when loading
-      // an already finished game, if the ending time of the game happened to fall on an
-      // alert time.
-      this.prevSeconds = seconds;
-      return;
-    }
-    if (seconds === this.prevSeconds) {
-      // The clock was re-rendered w/ no change, or the milliseconds changed but not the rest.
-      return;
-    }
-    this.prevSeconds = seconds;
-    if (this.extOpts.alertTimes.includes(seconds)) {
-      speak(`${seconds}`, this.voice);
-    }
+  const seconds = (Number(match[1]) * 60) + Number(match[2]);
+  if (prevSeconds == null) {
+    // It's the first render. If the starting time happens to fall on an alert marker,
+    // no need to alert at the start of the game. This also avoids alerting when loading
+    // an already finished game, if the ending time of the game happened to fall on an
+    // alert time.
+    prevSeconds = seconds;
+    return;
   }
-
-  tryInit() {
-    const timeEl = $(".rclock-bottom .time");
-    if (!timeEl) {
-      return false;
-    }
-
-    console.log("Lichess Countdown Timer is activated")
-    new MutationObserver(() => {
-      this.onTimeMutation(timeEl.innerText);
-    }).observe(timeEl, {childList: true});
-    return true;
+  if (seconds === prevSeconds) {
+    // The clock was re-rendered w/ no change, or the milliseconds changed but not the rest.
+    return;
+  }
+  prevSeconds = seconds;
+  if (OPTIONS.alertTimes.includes(seconds)) {
+    speak(`${seconds}`);
   }
 }
 
-async function main() {
-  console.log("Lichess Countdown Timer is initializing");
-  const extOpts = await getOptions();
-  const voices = await getVoices();
-  const voice = voices.find(voicePredicate(extOpts.voiceName));
-  const handler = new Handler(voice, extOpts);
+function tryInit() {
+  const timeEl = $(".rclock-bottom .time");
+  if (!timeEl) {
+    return false;
+  }
 
-  if (!handler.tryInit()) {
+  console.log("Lichess Countdown Timer is activated")
+  new MutationObserver(() => {
+    onTimeMutation(timeEl.innerText);
+  }).observe(timeEl, {childList: true});
+  return true;
+}
+
+function main() {
+  console.log("Lichess Countdown Timer is initializing");
+  if (!tryInit()) {
     const observer = new MutationObserver(() => {
-      if (handler.tryInit()) {
+      if (tryInit()) {
         observer.disconnect();
       }
     });
@@ -64,6 +82,6 @@ async function main() {
   }
 }
 
-main().then();
+main();
 
 export {};
